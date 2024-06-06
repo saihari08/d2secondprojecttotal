b4ae6e355f12808ec47c3c5d7d678006890713ff
saihari08 <bsaihari08@gmail.com>
2024-06-06 17:18:40 +0530

    Add files via upload

---
+++ secondreactapp/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+  <div>
+    <h1>ICC  Men's T20 World Cup </h1>
+    <img src="https://c.ndtvimg.com/2023-12/qhgfbe4o_t20-world-cup-2024-logo_625x300_07_December_23.jpg" width="500px">
+    </img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/ICC_Men%27s_T20_World_Cup_Trophy_at_COA_-_BugWarp_%2820%29_%28cropped%29.jpg/800px-ICC_Men%27s_T20_World_Cup_Trophy_at_COA_-_BugWarp_%2820%29_%28cropped%29.jpg" width="428px">
+     </img>
+      <h1>Fixtures for ICC T20 World Cup 2024 announced</h1>
+      <p>
+      The ICC on Friday, January 5, 2024, announced the fixtures for the 
+      upcoming ICC Men's T20 World Cup 2024 to be staged jointly at three 
+      venues in the USA and six in the Caribbean from 1st to 29th June. 
+      The 20-team event is split up into four groups consisting of five teams,
+      with the top two sides from each group then progressing to the Super Six
+      phase of the tournament ahead of the knockout semi-finals and final.
+      </p>
+    
+    <iframe width="400px" 
+     src="https://www.youtube.com/watch?v=aj0TXpTgbUM"></iframe>
+    </div>
+  );
+}
+
+export default App;
